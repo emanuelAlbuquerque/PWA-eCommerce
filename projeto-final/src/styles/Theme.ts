464ecc92cbdb19dc06067e78bd461dffbaed4aa7
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-15 23:26:07 -0300

    Arquivo com Tipografias e cores do projeto

---
+++ projeto-final/src/styles/Theme.ts
@@ -0,0 +1,62 @@
+export const Theme = {
+  Colors: {
+    tranparent: 'transparent',
+    primary: '#1B4B66',
+    primaryTint: '#639599',
+    error: '#B00020',
+    highLight: '#FF8C4B',
+    accent: '#F4F4F4',
+    dark: '#13101E',
+    bright: '#FFFFFF',
+    grey: '#F1F1F1',
+    lightText: '#B6B6B6',
+    highEmphasis: '#171520',
+    lowEmphasis: '#626262'
+  },
+  Typography: {
+    DisplayLarge: {
+      weight: '700',
+      size: '40px'
+    },
+    DisplayMedium: {
+      weight: '600',
+      size: '34px'
+    },
+    DisplaySmall: {
+      weight: '600',
+      size: '20px'
+    },
+    TitleMedium: {
+      weight: '500',
+      size: '14px'
+    },
+    TitleRegular: {
+      weight: '600',
+      size: '14px'
+    },
+    BodyMedium: {
+      weight: '500',
+      size: '16px'
+    },
+    BodyRegular: {
+      weight: '400',
+      size: '16px'
+    },
+    LabelLarge: {
+      weight: '400',
+      size: '14px'
+    },
+    LabelMedium: {
+      weight: '500',
+      size: '12px'
+    },
+    LabelSmall: {
+      weight: '600',
+      size: '12px'
+    },
+    Link: {
+      weight: '500',
+      size: '12px'
+    }
+  }
+}
