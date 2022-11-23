cf04c7b3721dadd9d7dc3bc6153dd82ec36ca2e2
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-23 14:34:15 -0300

    Adicionando novos tamnhos de fontes e mais cores

--- projeto-final/src/styles/Theme.ts
+++ projeto-final/src/styles/Theme.ts
@@ -4,14 +4,21 @@ export const Theme = {
     primary: '#1B4B66',
     primaryTint: '#639599',
     error: '#B00020',
+    lightRed: '#E21D1D',
     highLight: '#FF8C4B',
+    highDark: '#9C1C00',
+    high: '#97451F',
     accent: '#F4F4F4',
     dark: '#13101E',
     bright: '#FFFFFF',
     grey: '#F1F1F1',
     lightText: '#B6B6B6',
     highEmphasis: '#171520',
-    lowEmphasis: '#626262'
+    lowEmphasis: '#626262',
+    lightGray:'#D8D8D8',
+    Faded: '#7E7E7E',
+    orangeLight: '#FFDD9D',
+    rosa: '#A53F64'
   },
   Typography: {
     DisplayLarge: {
@@ -26,6 +33,10 @@ export const Theme = {
       weight: '600',
       size: '20px'
     },
+    TitleLarge: {
+      weight: '800',
+      size: '24px'
+    },
     TitleMedium: {
       weight: '500',
       size: '14px'
@@ -34,6 +45,14 @@ export const Theme = {
       weight: '600',
       size: '14px'
     },
+    TitleSmall: {
+      weight: '500',
+      size: '14px'
+    },
+    BodyLarge: {
+      weight: '800',
+      size: '16px',
+    },
     BodyMedium: {
       weight: '500',
       size: '16px'
@@ -41,6 +60,10 @@ export const Theme = {
     BodyRegular: {
       weight: '400',
       size: '16px'
+    },
+    BodySmall: {
+      weight: '800',
+      size: '14px'
     },
     LabelLarge: {
       weight: '400',
@@ -57,6 +80,18 @@ export const Theme = {
     Link: {
       weight: '500',
       size: '12px'
+    },
+    LinkSmall: {
+      weight: '500',
+      size: '10px'
+    },
+    LinkMedium: {
+      weight: '400',
+      size: '10px'
+    },
+    card: {
+      weight: '600',
+      size: '24px'
     }
   }
 }
