947e39c6958c624aa0cb6677a17ef719ee0e3d3f
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-23 14:23:57 -0300

    Adicionando novas imagens do projeto

--- projeto-final/src/assets/Icons/General/ArrowRight.tsx
+++ projeto-final/src/assets/Icons/General/ArrowRight.tsx
@@ -1,4 +1,7 @@
-export function ArrowRight(){
+import { IconProps } from "../Product/SkinCare";
+
+
+export function ArrowRight({className}: IconProps){
   return(
     <svg
       width={16}
@@ -6,6 +9,7 @@ export function ArrowRight(){
       viewBox="0 0 16 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={className}
     >
       <path
         d="M1 5.5h14m0 0L10.5 1M15 5.5L10.5 10"
@@ -13,7 +17,6 @@ export function ArrowRight(){
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="icon"
       />
     </svg>
   )
