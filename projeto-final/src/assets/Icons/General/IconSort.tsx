0f62f87253f5050a6562e7ed0a384559cd0106e4
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-25 01:16:29 -0300

    Adicionando novos icones que apareceu nas telas e adicionando as logos das marcas

---
+++ projeto-final/src/assets/Icons/General/IconSort.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from "../Product/SkinCare";
+
+export function IconSort({className}:IconProps){
+  return (
+    <svg
+      width={20}
+      height={16}
+      viewBox="0 0 20 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path fill="#B6B6B6" d="M0 0H4.44444V4.57143H0z" />
+      <path fill="#B6B6B6" d="M0 5.71436H4.44444V10.28579H0z" />
+      <path fill="#B6B6B6" d="M0 11.4287H4.44444V16.00013H0z" />
+      <path fill="#B6B6B6" d="M5.55554 0H19.99994V4.57143H5.55554z" />
+      <path fill="#B6B6B6" d="M5.55554 5.71436H19.99994V10.28579H5.55554z" />
+      <path fill="#B6B6B6" d="M5.55554 11.4287H19.99994V16.00013H5.55554z" />
+    </svg>
+  )
+}
--- projeto-final/src/assets/Icons/Navigation/HomeIcon.tsx
+++ projeto-final/src/assets/Icons/Navigation/HomeIcon.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Theme } from "../../../styles/Theme"
 import { IconProps } from "../Product/SkinCare"
 
-export function Home({color = 'none'}: IconProps) {
+export function HomeIcon({color = 'none'}: IconProps) {
   return (
     <svg
       width={20}
