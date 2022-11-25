a0d554f9ae9b81898bb6f9da6a5d3e7fb846ea99
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-25 11:56:07 -0300

    Adicionando o react-dom no storybook

--- projeto-final/src/components/BottomNavigation/BottomNavigation.stories.tsx
+++ projeto-final/src/components/BottomNavigation/BottomNavigation.stories.tsx
@@ -1,10 +1,12 @@
 import { BottomNavigation, BottomNavigationProps } from "./BottomNavigation";
 import { Meta, Story } from "@storybook/react";
 import { GlobalStyle } from "../../styles/Global";
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 export default {
   title: 'BottomNavigation/BottomNavigation',
   component: BottomNavigation,
+  decorators: [withRouter],
 } as Meta<BottomNavigationProps>
 
 export const BottomNavigationLight: Story<BottomNavigationProps> = () => (
--- projeto-final/src/components/Header/Header.stories.tsx
+++ projeto-final/src/components/Header/Header.stories.tsx
@@ -1,9 +1,11 @@
 import { GlobalStyle } from "../../styles/Global";
 import { Header } from "./Header";
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 export default {
   title: 'Header/Header',
   component: Header,
+  decorators: [withRouter],
 }
 
 export const Default = () => (
