cf91bd055683f9ddb1bc1c44d89adbfa5c243f0b
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-25 04:57:08 -0300

    Adicionando as rotas

--- projeto-final/src/App.tsx
+++ projeto-final/src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CategoryPage } from "./Pages/CategoryPage/CategoryPage"
 import { Home } from "./Pages/Home/Home"
 
 function App(){
   return (
-    <CategoryPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='/categoryPage' element={<CategoryPage />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
