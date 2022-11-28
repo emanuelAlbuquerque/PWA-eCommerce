24dc62e116500f1561108896b9b54c8a9950f332
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-28 15:05:05 -0300

    Alterando a função de lugar

--- projeto-final/src/components/BottomNavigation/ButtonNavigationItem/NavigationItem.tsx
+++ projeto-final/src/components/BottomNavigation/ButtonNavigationItem/NavigationItem.tsx
@@ -12,35 +12,6 @@ export interface NavigationItemProps{
 }
 
 
-export function buttonActive(e: React.MouseEvent<HTMLElement, MouseEvent>) {
-  const paiElement = e.currentTarget.parentElement?.children
-
-  if(paiElement){
-    Array.from(paiElement).forEach((filho) => {//Tranforma a coleção em array, e reseta as classes.
-        filho.children[0].classList.remove('active')
-        filho.children[0].classList.add('disable')
-      })
-  }
-  
-  e.currentTarget.children[0].classList.add('active')
-  // navegaClick(e)
-}
-
-// function navegaClick(e: React.MouseEvent<HTMLElement, MouseEvent>){
-//     if(e.currentTarget.classList.contains('home')){
-     
-//     }
-//     if(e.currentTarget.classList.contains('profile')){
-//       console.log('pagina profile')
-//     }
-//     if(e.currentTarget.classList.contains('bag')){
-//       console.log('/categoryPage')
-//     }
-//     if(e.currentTarget.classList.contains('categories')){
-//       console.log('pagina categories')
-//     }
-// }
-
 export function NavigationItem({ className, icon, text, id, theme, active = 'disable', onClick }: NavigationItemProps){
 
 
