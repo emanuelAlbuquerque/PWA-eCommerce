ca179799cc1fe98012c42b4c293a7d7fb44b2f49
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-28 15:17:25 -0300

    Criando um json para os chekboc

--- projeto-final/src/App.tsx
+++ projeto-final/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CategoryPage } from "./Pages/CategoryPage/CategoryPage"
 import { Home } from "./Pages/Home/Home"
+import { ProductPage } from "./Pages/ProductPage/ProductPage"
 
 function App(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-          <Route path='/categoryPage' element={<CategoryPage />} />
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/categoryPage" element={<CategoryPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
   )
