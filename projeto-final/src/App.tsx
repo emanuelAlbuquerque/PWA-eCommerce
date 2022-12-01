74c4f6744ed28d6f007bd4a422deffe16777f0a1
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-01 14:51:52 -0300

    Adicionando rotas para a pagina de myCart e Checkout e mudando o parametro da pagina product

--- projeto-final/src/App.tsx
+++ projeto-final/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CategoryPage } from "./Pages/CategoryPage/CategoryPage"
+import { CheckoutPage } from "./Pages/CheckoutPage/CheckoutPage"
 import { Home } from "./Pages/Home/Home"
+import { MyCart } from "./Pages/MyCart/MyCart"
 import { ProductPage } from "./Pages/ProductPage/ProductPage"
 
 function App(){
@@ -9,7 +11,9 @@ function App(){
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categoryPage" element={<CategoryPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/product/:idProduto" element={<ProductPage />} />
+        <Route path="/myCarty" element={<MyCart />} />
+        <Route path="/checkoutPage" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
   )
