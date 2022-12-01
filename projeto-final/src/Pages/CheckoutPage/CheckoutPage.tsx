f3b0fd3be0ea43b6c95c133c759af39357b63894
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-01 14:52:30 -0300

    Criando arquivos para começar a fazer a pagina checkout

---
+++ projeto-final/src/Pages/CheckoutPage/CheckoutPage.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header/Header";
+import { PageTitle } from "../MyCart/style";
+
+export function CheckoutPage(){
+  return(
+    <>
+      <Header />
+
+      <PageTitle>
+        Checkout
+      </PageTitle>
+
+    </>
+  )
+}
