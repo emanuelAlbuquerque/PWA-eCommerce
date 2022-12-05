fb3c4621930d71e5013ed3322da1d645b61f7c54
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-04 22:17:37 -0300

    Adicionando novas imagens do projeto

--- projeto-final/src/assets/Icons/General/Plus.tsx
+++ projeto-final/src/assets/Icons/General/Plus.tsx
@@ -13,7 +13,7 @@ export function Plus(){
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="icon"
+        className="iconStroke"
       />
     </svg>
   )
