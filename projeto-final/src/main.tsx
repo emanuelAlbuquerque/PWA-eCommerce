057538718c3c81cb1e2ca823f7235a5dda022d8e
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-04 22:17:06 -0300

    Tentano colocar o Theme.Provider

--- projeto-final/src/main.tsx
+++ projeto-final/src/main.tsx
@@ -5,7 +5,7 @@ import { GlobalStyle } from './styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 )
