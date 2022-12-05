f577cdeef162c2f476e01a54c4ac2f74ced4dc1d
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-04 22:16:36 -0300

    Adicionando novas rotas

--- projeto-final/src/App.tsx
+++ projeto-final/src/App.tsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AddNewAddress } from "./Pages/AddNewAdress/AddNewAdress"
+import { BagMobile } from "./Pages/BagMobile/BagMobile"
+import { BagVazia } from "./Pages/BagVazia/BagVazia"
 import { CategoryPage } from "./Pages/CategoryPage/CategoryPage"
 import { CheckoutPage } from "./Pages/CheckoutPage/CheckoutPage"
 import { Home } from "./Pages/Home/Home"
 import { MyCart } from "./Pages/MyCart/MyCart"
+import { OrderSummaryPage } from "./Pages/OrderSummary/OrderSummary"
+import { PaymentsPage } from "./Pages/PaymentsPage/PaymentsPage"
 import { ProductPage } from "./Pages/ProductPage/ProductPage"
 
 function App(){
@@ -14,6 +19,11 @@ function App(){
         <Route path="/product/:idProduto" element={<ProductPage />} />
         <Route path="/myCarty" element={<MyCart />} />
         <Route path="/checkoutPage" element={<CheckoutPage />} />
+        <Route path="/bagMobile" element={<BagMobile />} />
+        <Route path="/bagVazia" element={<BagVazia />} />
+        <Route path="/orderSummary" element={<OrderSummaryPage />} />
+        <Route path="/addNewAddress" element={<AddNewAddress />} />
+        <Route path="/payments" element={<PaymentsPage />} />
       </Routes>
     </BrowserRouter>
   )
