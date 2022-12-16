78c7d0642031c2eb2f292889551468b59b2f72ef
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-16 00:11:01 -0300

    Adicionando a mensagem de erro

--- projeto-final/src/components/FormControl/style.ts
+++ projeto-final/src/components/FormControl/style.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { Theme } from "../../styles/Theme";
 
-interface ContainerProps{
-  text: string 
+interface ContainerProps {
+  text: string
   disable: boolean
   error: boolean
   variant: "action" | "icons" | "default" | "active"
@@ -16,32 +16,39 @@ export const Conteiner = styled.div<ContainerProps>`
     font-weight: ${Theme.Typography.Link.weight};
   }
 
+  .mensagemError{
+    margin-top: .3rem;
+    color: ${Theme.Colors.error};
+    font-size: ${Theme.Typography.Link.size};
+    font-weight: ${Theme.Typography.Link.weight};
+  }
+
   .containerInput{
     width: 100%;
     height: 56px;
     background-color: ${Theme.Colors.grey};
 
-    ${({ disable }) => disable 
-                        ? `background-color: ${Theme.Colors.lightText};` 
-                        : `background-color: ${Theme.Colors.grey};`}
+    ${({ disable }) => disable
+    ? `background-color: ${Theme.Colors.lightText};`
+    : `background-color: ${Theme.Colors.grey};`}
 
     padding: 0 1rem;
     display: flex;
     align-items: center;
 
-    ${({ error }) => error && `border: 1px solid ${Theme.Colors.error};` }
+    ${({ error }) => error && `border: 1px solid ${Theme.Colors.error};`}
 
 
 
     ${({ error, text, variant }) => {
-      if(variant !== 'active'){
-        if (text.length > 0) {
-          return `
+    if (variant !== 'active') {
+      if (text.length > 0) {
+        return `
             border: 1px solid ${error ? `${Theme.Colors.error}` : `${Theme.Colors.primary}`};
           `
-        }
       }
-    }}
+    }
+  }}
     
 
     ${({ variant, error }) => variant !== 'active' && `
@@ -60,7 +67,7 @@ export const Conteiner = styled.div<ContainerProps>`
       &:focus-within .input input{
         margin-top: 1rem;
       }`
-    }
+  }
 
 
     .iconLeft{
@@ -90,41 +97,41 @@ export const Conteiner = styled.div<ContainerProps>`
         font-weight: ${Theme.Typography.BodyMedium.weight};
         transition: all .2s;
 
-        ${({text, variant}) => {
-          if(variant !== 'active'){
-            if(text.length > 0){
-              return`
+        ${({ text, variant }) => {
+    if (variant !== 'active') {
+      if (text.length > 0) {
+        return `
                 margin-top: 1rem;
               `
-            }
-          }
-        }}
+      }
+    }
+  }}
       }
 
       label{
         position: absolute;
         pointer-events: none;
 
-        ${({text, error, variant}) => {
-          if(variant !== 'active'){
-            if(text.length > 0){
-              return `
+        ${({ text, error, variant }) => {
+    if (variant !== 'active') {
+      if (text.length > 0) {
+        return `
               color: ${error ? `${Theme.Colors.error}` : `${Theme.Colors.primary}`};
               font-size: ${Theme.Typography.LabelMedium.size};
               font-weight: ${Theme.Typography.LabelMedium.weight};
               top: -7px;
               `
-            }else{
-              return `
+      } else {
+        return `
                 top: 0;
                 left: 0;
                 color: ${Theme.Colors.lowEmphasis};
                 font-size: ${Theme.Typography.BodyMedium.size};
                 font-weight: ${Theme.Typography.BodyMedium.weight};
               `
-            }
-          }
-        }}
+      }
+    }
+  }}
 
         transition: all .2s;
       }
